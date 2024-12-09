59a65aaf61891366ede8bf47fddf7454c6744ef0
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-12-09 09:14:15 -0600

    Setup init files.
    Created basic html and js.
    Began using js as testing ground.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# template-repo
+# TOP-weather-app
 
-Dedicated template repo for init new projects
+https://www.theodinproject.com/lessons/node-path-javascript-weather-app
+
+Practice promises and async/await
+
+todo:
+
+<!--
+    - You should be able to search for a specific location
+    - toggle displaying the data in Fahrenheit or Celsius
+    - You should change the look of the page based on the data
+        -  maybe by changing the color of the background or by adding images that describe the weather. (You could even use the Giphy API to find appropriate weather-related gifs and display them).
+    - use promises or async/await
+    - Write the functions that process the JSON data
+        -  return an object with only the data you require for your app.
+    - Set up a form that will let users input their location and will fetch the weather info
+    - Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API. Use DevTools to test for low-end devices.
+ -->
--- src/index.js
+++ src/index.js
@@ -7,5 +7,33 @@ import "./style.css";
 
 // ====================================== Init ====================================== //
 
-//test
-console.log("test");
+const weatherResultsElm = document.querySelector("#weather-results");
+const searchWeatherBtn = document.querySelector("#weather-search-btn");
+const locationInputElm = document.querySelector("#location-input");
+
+//need learn location api
+async function getWeather(locationInput) {
+  try {
+    const weatherData = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationInput}/2024-12-08?key=3LTVBASAQ7PVLZQWD6V4TN9MQ`
+    );
+    const weatherJSON = await weatherData.json();
+    return weatherJSON.days[0].description;
+  } catch (err) {
+    console.log(err);
+    return "will fix i promise";
+    // throw "will fix i promise";
+  }
+}
+
+const getLocationInput = () => locationInputElm.value;
+
+//get today date
+
+searchWeatherBtn.addEventListener("click", (event) => {
+  //stop form submit
+  event.preventDefault();
+
+  const weatherJSON = getWeather(getLocationInput());
+  weatherJSON.then(console.log);
+});
