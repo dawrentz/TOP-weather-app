d36c81076134af286bbacfb44a82e50dc4e7b777
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-12-10 13:45:56 -0600

    Render Mason quote to page.
    Modified getEmoji() to return a emoji description also because, for some
    reason, the passed through emoji wouldn't NOT be recogonized as the same
    emoji. Believe it has something to do with the emoji being found in
    another function (things seemed to work if the emoji variable is global).
    
    This is what would happen in getMasonQuote():
        console.log(emoji); //returns "☁️"
        console.log(emoji === "☁️"); //returns "false"
    idk...
    
    Solution was to go back to getEmoji() and return an "emoji descriptor"
    that can be use to look up the quote instead of the actual emoji.
    
    Needs styling.

--- src/index.js
+++ src/index.js
@@ -20,3 +20,223 @@ weatherReportForm.addEventListener("submit", (event) => {
   // getWeatherMod.reportWeather();
   renderMod.showWeatherReport();
 });
+
+//testing
+// function getEmoji() {
+//   const emoji = "☁️";
+//   return emoji;
+// }
+
+// const emoji = getEmoji();
+
+// //test
+// console.log(emoji);
+
+// function getQuote(emoji) {
+//   let quote;
+//   let quoteSrc;
+
+//   if (emoji === "🚫") {
+//     quote = `"What the hell are you doin'?"`;
+//     quoteSrc = `Mason Storm: The Comeback II`;
+//   }
+
+//   if (emoji === "☔") {
+//     quote = `"Sara... Sara loved the rain..."`;
+//     quoteSrc = `Mason Storm (Director's Cut)`;
+//   }
+
+//   if (emoji === "💨") {
+//     quote = `"Gimme a jacket. Leather. Black."`;
+//     quoteSrc = `Mason Storm: UNLEASHED (S1E8)`;
+//   }
+
+//   if (emoji === "❄️") {
+//     quote = `"It'll be a cold day in Honolulu before you kill me!"`;
+//     quoteSrc = `Mason Storm: A Time to Die`;
+//   }
+
+//   if (emoji === "🌫️") {
+//     quote = `"This haze really takes me back... keep your eyes peeled..."`;
+//     quoteSrc = `Mason Storm: Return to Vietnam`;
+//   }
+
+//   if (emoji === "☁️") {
+//     quote = `"I can't see it... the vision... it's too cloudy!"`;
+//     quoteSrc = `cameo from Taken 8: Kidnapped in Space`;
+//   }
+
+//   if (emoji === "🌀") {
+//     quote = `"I don't think were in Brooklyn anymore, Toto"`;
+//     quoteSrc = `Mason Storm 2: Lost in Time (1999)`;
+//   }
+
+//   if (emoji === "☀️") {
+//     quote = `"The sun'll never set on your grave... Cause there'll be nothin' left!"`;
+//     quoteSrc = `Mason Storm: The Final Chapter IV`;
+//   }
+
+//   if (emoji === "🌄") {
+//     quote = `"I'm here to clear your conscious... with my .45 automatic..."`;
+//     quoteSrc = `2 Seconds 2 Midnight 2`;
+//   }
+
+//   if (emoji === "⛈️") {
+//     quote = `There's a STORM comin'!!!"`;
+//     quoteSrc = `Mason Storm (Theatrical Cut)`;
+//   }
+
+//   return { quote, quoteSrc };
+// }
+
+// const { quote, quoteSrc } = getQuote(emoji);
+
+// console.log(quote);
+// console.log(quoteSrc);
+
+function getEmoji(conditions) {
+  const formatCond = conditions.toLowerCase();
+  //init to error
+  let emoji = "🚫";
+
+  //rain
+  if (
+    formatCond.includes("rain") ||
+    formatCond.includes("drizzle") ||
+    formatCond.includes("precipitation")
+  ) {
+    emoji = "☔";
+  }
+
+  //wind
+  if (formatCond.includes("wind") || formatCond.includes("squall")) {
+    emoji = "️💨";
+  }
+
+  //snow
+  if (
+    formatCond.includes("snow") ||
+    formatCond.includes("hail") ||
+    formatCond.includes("ice")
+  ) {
+    emoji = "❄️";
+  }
+
+  //fog
+  if (formatCond.includes("fog")) {
+    emoji = "🌫️";
+  }
+
+  //smoke
+  if (formatCond.includes("dust") || formatCond.includes("smoke")) {
+    emoji = "️‍🔥";
+  }
+
+  //cloud
+  if (
+    formatCond.includes("cloud") ||
+    formatCond.includes("overcast") ||
+    formatCond.includes("cover")
+  ) {
+    emoji = "️☁️";
+  }
+
+  //tornado
+  if (formatCond.includes("tornado") || formatCond.includes("hurricane")) {
+    emoji = "️🌀";
+  }
+
+  //sun
+  if (formatCond.includes("sun")) {
+    emoji = "️☀️";
+  }
+
+  //clear
+  if (formatCond.includes("clear")) {
+    emoji = "️🌄";
+  }
+
+  //storm
+  //"storm" takes presidence over "rainstorm", "snowstorm" or other storms (for Mason quote)
+  if (formatCond.includes("storm") || formatCond.includes("thunder")) {
+    emoji = "⛈️";
+  }
+
+  return emoji;
+}
+
+function getMasonQuote(emoji) {
+  let quote;
+  let quoteSrc;
+
+  //test
+  console.log(emoji);
+  console.log(emoji === "☁️");
+
+  //error
+  if (emoji === "🚫") {
+    quote = `"What the hell are you doin'?"`;
+    quoteSrc = `Mason Storm: The Comeback II`;
+  }
+
+  //rain
+  if (emoji === "☔") {
+    quote = `"Sara... Sara loved the rain..."`;
+    quoteSrc = `Mason Storm (Director's Cut)`;
+  }
+
+  //wind
+  if (emoji === "💨") {
+    quote = `"Gimme a jacket. Leather. Black."`;
+    quoteSrc = `Mason Storm: UNLEASHED (S1E8)`;
+  }
+
+  //snow
+  if (emoji === "❄️") {
+    quote = `"It'll be a cold day in Honolulu before you kill me!"`;
+    quoteSrc = `Mason Storm: A Time to Die`;
+  }
+
+  //fog
+  if (emoji === "🌫️") {
+    quote = `"This haze really takes me back... keep your eyes peeled..."`;
+    quoteSrc = `Mason Storm: Return to Vietnam`;
+  }
+
+  //cloud
+  if (emoji === "☁️") {
+    quote = `"I can't see it... the vision... it's too cloudy!"`;
+    quoteSrc = `Taken 8: Kidnapped in Space (cameo)`;
+  }
+
+  //tornado
+  if (emoji === "🌀") {
+    quote = `"I don't think were in Brooklyn anymore, Toto"`;
+    quoteSrc = `Mason Storm 2: Lost in Time (1999)`;
+  }
+
+  //sun
+  if (emoji === "☀️") {
+    quote = `"The sun'll never set on your grave... Cause there'll be nothin' left!"`;
+    quoteSrc = `Mason Storm: The Final Chapter IV`;
+  }
+
+  //clear
+  if (emoji === "🌄") {
+    quote = `"I'm here to clear your conscious... with my .45 automatic..."`;
+    quoteSrc = `2 Seconds 2 Midnight 2`;
+  }
+
+  //storm
+  if (emoji === "⛈️") {
+    quote = `There's a STORM comin'!!!"`;
+    quoteSrc = `Mason Storm (Theatrical Cut)`;
+  }
+
+  return { quote, quoteSrc };
+}
+
+const weatherEmoji = getEmoji("sun");
+console.log(weatherEmoji);
+const weatherQuote = getMasonQuote(weatherEmoji);
+console.log(weatherQuote);
--- src/renderMod.js
+++ src/renderMod.js
@@ -8,6 +8,7 @@ const reportAddressElm = document.querySelector("#report-address");
 const tempuratureReportElm = document.querySelector("#temp-report");
 const weatherEmojiElm = document.querySelector("#weather-emoji");
 const masonQuoteElm = document.querySelector("#mason-quote");
+const masonQuoteSrcElm = document.querySelector("#mason-quote-src");
 const locationInputElm = document.querySelector("#location-input");
 const conditionReportElm = document.querySelector("#condition-report");
 
@@ -23,21 +24,35 @@ export function showWeatherReport() {
 
 function handleWeatherReport(weatherObj) {
   const { conditions, temp, resolvedAddress, unitGroup } = weatherObj;
-  const emoji = getEmoji(conditions);
+  const { emoji, emojiDescript } = getEmoji(conditions);
+  const { quote, quoteSrc } = getMasonQuote(emojiDescript);
 
-  renderWeatherInfo(conditions, temp, resolvedAddress, unitGroup, emoji);
+  renderWeatherInfo(
+    conditions,
+    temp,
+    resolvedAddress,
+    unitGroup,
+    emoji,
+    quote,
+    quoteSrc
+  );
 }
 
+//export out for demo?
 function renderWeatherInfo(
   conditions,
   temp,
   resolvedAddress,
   unitGroup,
-  emoji
+  emoji,
+  quote,
+  quoteSrc
 ) {
   conditionReportElm.textContent = conditions;
   reportAddressElm.textContent = resolvedAddress;
   weatherEmojiElm.textContent = emoji;
+  masonQuoteElm.textContent = quote;
+  masonQuoteSrcElm.textContent = quoteSrc;
 
   if (unitGroup === "us") {
     tempuratureReportElm.textContent = `${temp} F°`;
@@ -50,6 +65,7 @@ function getEmoji(conditions) {
   const formatCond = conditions.toLowerCase();
   //init to error
   let emoji = "🚫";
+  let emojiDescript = "error";
 
   //rain
   if (
@@ -58,11 +74,13 @@ function getEmoji(conditions) {
     formatCond.includes("precipitation")
   ) {
     emoji = "☔";
+    emojiDescript = "rain";
   }
 
   //wind
   if (formatCond.includes("wind") || formatCond.includes("squall")) {
     emoji = "️💨";
+    emojiDescript = "wind";
   }
 
   //snow
@@ -72,16 +90,19 @@ function getEmoji(conditions) {
     formatCond.includes("ice")
   ) {
     emoji = "❄️";
+    emojiDescript = "snow";
   }
 
   //fog
   if (formatCond.includes("fog")) {
     emoji = "🌫️";
+    emojiDescript = "fog";
   }
 
   //smoke
   if (formatCond.includes("dust") || formatCond.includes("smoke")) {
     emoji = "️‍🔥";
+    emojiDescript = "smoke";
   }
 
   //cloud
@@ -91,30 +112,119 @@ function getEmoji(conditions) {
     formatCond.includes("cover")
   ) {
     emoji = "️☁️";
+    emojiDescript = "cloud";
   }
 
   //tornado
   if (formatCond.includes("tornado") || formatCond.includes("hurricane")) {
     emoji = "️🌀";
+    emojiDescript = "tornado";
   }
 
   //sun
   if (formatCond.includes("sun")) {
     emoji = "️☀️";
+    emojiDescript = "sun";
   }
 
   //clear
   if (formatCond.includes("clear")) {
     emoji = "️🌄";
+    emojiDescript = "clear";
   }
 
   //storm
   //"storm" takes presidence over "rainstorm", "snowstorm" or other storms (for Mason quote)
   if (formatCond.includes("storm") || formatCond.includes("thunder")) {
     emoji = "⛈️";
+    emojiDescript = "storm";
   }
 
-  return emoji;
+  return { emoji, emojiDescript };
+}
+
+function getMasonQuote(emoji) {
+  let quote;
+  let quoteSrc;
+
+  //for some reason, the passed through emoji will NOT be recogonized as the same emoji
+  //going back to getEmoji() and returning an "emoji emojiDescriptor" that can be looked the quote up here
+  //it has something to do with the emoji being found in another function, things seem to work if the emoji variable is global
+  //try it out here (need a cloudy location)
+  //   console.log(emoji); //returns ☁️
+  //   console.log(emoji === "☁️"); //returns false
+
+  //error
+  //   if (emoji === "🚫") {
+  if (emoji === "error") {
+    quote = `"What the hell are you doin'?"`;
+    quoteSrc = `Mason Storm: The Comeback II`;
+  }
+
+  //rain
+  //   if (emoji === "☔") {
+  if (emoji === "rain") {
+    quote = `"Sara... Sara loved the rain..."`;
+    quoteSrc = `Mason Storm (Director's Cut)`;
+  }
+
+  //wind
+  //   if (emoji === "💨") {
+  if (emoji === "wind") {
+    quote = `"Gimme a jacket. Leather. Black."`;
+    quoteSrc = `Mason Storm: UNLEASHED (S1E8)`;
+  }
+
+  //snow
+  //   if (emoji === "❄️") {
+  if (emoji === "snow") {
+    quote = `"It'll be a cold day in Honolulu before you kill me!"`;
+    quoteSrc = `Mason Storm: A Time to Die`;
+  }
+
+  //fog
+  //   if (emoji === "🌫️") {
+  if (emoji === "fog") {
+    quote = `"This haze really takes me back... keep your eyes peeled..."`;
+    quoteSrc = `Mason Storm: Return to Vietnam`;
+  }
+
+  //cloud
+  //   if (emoji === "☁️") {
+  if (emoji === "cloud") {
+    quote = `"I can't see it... the vision... it's too cloudy!"`;
+    quoteSrc = `Taken 8: Kidnapped in Space (cameo)`;
+  }
+
+  //tornado
+  //   if (emoji === "🌀") {
+  if (emoji === "tornado") {
+    quote = `"I don't think were in Brooklyn anymore, Toto"`;
+    quoteSrc = `Mason Storm 2: Lost in Time (1999)`;
+  }
+
+  //sun
+  //   if (emoji === "☀️") {
+  if (emoji === "sun") {
+    quote = `"The sun'll never set on your grave... Cause there'll be nothin' left!"`;
+    quoteSrc = `Mason Storm: The Final Chapter IV`;
+  }
+
+  //clear
+  //   if (emoji === "🌄") {
+  if (emoji === "clear") {
+    quote = `"I'm here to clear your conscious... with my .45 automatic..."`;
+    quoteSrc = `2 Seconds 2 Midnight 2`;
+  }
+
+  //storm
+  //   if (emoji === "⛈️") {
+  if (emoji === "storm") {
+    quote = `There's a STORM comin'!!!"`;
+    quoteSrc = `Mason Storm (Theatrical Cut)`;
+  }
+
+  return { quote, quoteSrc };
 }
 
 //no need for abstract?
@@ -130,7 +240,6 @@ function getLocationInput() {
 // rain
 // drizzle
 // precipitation
-// "Sara... Sara loved the rain..." - from Mason Storm (Director's Cut)
 
 // storm
 // thunder
